b6a09a180802bc84e4e2fcf0b18656ad29e912b6
Ludovico Lemma <89784228+lwdovico@users.noreply.github.com>
2025-03-09 00:00:18 +0100

    First commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM pytorch/pytorch:2.6.0-cuda12.4-cudnn9-devel
+RUN pip install uv
+
+RUN apt update && \
+    apt install -y espeak-ng ffmpeg git && \
+    rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+RUN git clone https://github.com/Zyphra/Zonos.git .
+RUN uv pip install --system -e . && uv pip install --system -e .[compile]
+
+COPY . ./
---
+++ main.py
@@ -0,0 +1,56 @@
+import torch
+import torchaudio
+from zonos.model import Zonos
+from zonos.conditioning import make_cond_dict
+from zonos.utils import DEFAULT_DEVICE as device
+
+from pydub import AudioSegment
+
+from tqdm import tqdm
+import argparse
+import tempfile
+import re
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-text", type=str)
+    parser.add_argument("--speaker", type=str, default="assets/exampleaudio.mp3")
+    parser.add_argument("--language", type=str, default="en-us")
+    parser.add_argument("--output-path", type=str, default="output.mp3")
+    parser.add_argument("--seed", type=int, default=42)
+    args = parser.parse_args()
+
+    model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-transformer", device=device)
+
+    wav, sampling_rate = torchaudio.load(args.speaker)
+    speaker = model.make_speaker_embedding(wav, sampling_rate)
+
+    torch.manual_seed(args.seed)
+    
+    if os.path.exists(args.input_text):
+        print("Reading from file")
+        with open(args.input_text, "r") as f:
+            source_text = f.read()
+    elif ' ' in args.input_text or '\n' in args.input_text:
+        print("Reading from string")
+        source_text = args.input_text
+
+    texts = [s.strip() for s in re.split(r'(?<=[.!?])\s+|\n+', source_text) if s.strip()]
+
+    merged_audio = AudioSegment.silent(duration=0)
+
+    for sentence in tqdm(texts):
+        cond_dict = make_cond_dict(text=sentence, speaker=speaker, language=args.language)
+        conditioning = model.prepare_conditioning(cond_dict)
+        codes = model.generate(conditioning, progress_bar = False)
+        wavs = model.autoencoder.decode(codes).cpu()
+
+        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as f:
+            torchaudio.save(f.name, wavs[0], model.autoencoder.sampling_rate)
+            merged_audio += AudioSegment.from_wav(f.name) + AudioSegment.silent(duration=500)
+
+    with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as f:
+        merged_audio.export(f.name, format='mp3')
+        print("Normalizing audio...")
+        os.system(f'mkdir -p "$(dirname {args.output_path})" && ffmpeg -i {f.name} -af "loudnorm" {args.output_path}')
